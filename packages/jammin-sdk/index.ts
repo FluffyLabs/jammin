67f6421230733c6aef68e25726e1e4e0dfcd1102
HeyImStas <beebx4f@protonmail.com>
2026-02-10 14:48:50 +0100

    clean

--- packages/jammin-sdk/index.ts
+++ packages/jammin-sdk/index.ts
@@ -1,5 +1,3 @@
-import { BytesBlob } from "@typeberry/lib/bytes";
-
 export * as block from "@typeberry/lib/block";
 export * as codec from "@typeberry/lib/codec";
 export * as config from "@typeberry/lib/config";
@@ -12,52 +10,3 @@ export * from "./genesis-state-generator.js";
 export * from "./testing-helpers.js";
 export * from "./types.js";
 export * from "./utils/index.js";
-
-export function getSDKInfo() {
-  return {
-    name: "@fluffylabs/jammin-sdk",
-    version: "0.0.2",
-    description: "JAM SDK for e2e integration tests and object encoding",
-  };
-}
-
-/**
- * Create `BytesBlob` by converting given UTF-u encoded string into bytes.
- *
- * @example
- * ```typescript
- * const blob = StringToBytes("Hello, World!");
- * ```
- */
-export function StringToBytes(data: string): BytesBlob {
-  return BytesBlob.blobFromString(data);
-}
-
-/**
- * Create `BytesBlob` from an array of bytes.
- *
- * @example
- * ```typescript
- * const blob = NumbersToBytes([1, 2, 3]);
- * ```
- */
-export function NumbersToBytes(data: number[]): BytesBlob {
-  return BytesBlob.blobFromNumbers(data);
-}
-
-/**
- * Create `BytesBlob` from hex-encoded bytes string.
- *
- * @example
- * ```typescript
- * const blob = HexToBytes("deadface");
- * // or
- * const blob = HexToBytes("0x1337beef");
- * ```
- */
-export function HexToBytes(hex: string): BytesBlob {
-  if (!hex.startsWith("0x")) {
-    return BytesBlob.parseBlobNoPrefix(hex);
-  }
-  return BytesBlob.parseBlob(hex);
-}
